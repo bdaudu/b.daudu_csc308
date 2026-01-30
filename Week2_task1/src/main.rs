165f7c0d1746f4c16779042edc8addf30242a3fd
bdaudu <basil.daudu@pau.edu.ng>
2026-01-30 02:46:16 +0100

    Week 2 Task 1

---
+++ Week2_task1/src/main.rs
@@ -0,0 +1,16 @@
+fn add_note(msg:&mut String){
+    msg.push_str(", world!");
+}
+
+fn show_message(msg: &String){
+    println!("Current message: {}", msg);
+}
+
+fn main() {
+    let mut message = String::from("Hello");
+    
+    show_message(&message);
+    add_note(&mut message);
+
+    println!("Final message:{}", message);
+}
---
+++ Week2_task1/target/debug/deps/Week2_task1-25dcf435b5913c5d.d
@@ -0,0 +1,5 @@
+/home/basildaudu/csc308/Week2_task1/target/debug/deps/Week2_task1-25dcf435b5913c5d.d: src/main.rs
+
+/home/basildaudu/csc308/Week2_task1/target/debug/deps/Week2_task1-25dcf435b5913c5d: src/main.rs
+
+src/main.rs:
